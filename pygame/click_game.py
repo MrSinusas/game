5b5e2c8d5ffd8eab27729a959dfa25097c25eb96
MrSinusas <113166959+MrSinusas@users.noreply.github.com>
2023-02-08 07:21:11 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,16 +1,1 @@
-my_character = Actor('character')
-my_character.pos = 100, 56
-
-WIDTH = 500
-HEIGHT = my_character.height + 20
-
-def draw():
-    screen.clear()
-    screen.fill((255, 255, 255))
-    my_character.draw()
-
-def update():
-    my_character.left = my_character.left + 2
-    if my_character.left > WIDTH:
-        my_character.right = 0
 
