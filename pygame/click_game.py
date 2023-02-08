db8715c0822beb602d5fab3d8235186f6f63017b
MrSinusas <113166959+MrSinusas@users.noreply.github.com>
2023-02-08 10:09:09 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,14 +1,14 @@
 blob = Actor('character')
-blob.topright = 0, 10
+blob.pos = 100, 50
 
 WIDTH = 500
 HEIGHT = blob.height + 20
 
 def draw():
-    screen.clear()
+    screen.fill((255, 0, 0))
     blob.draw()
 
 def update():
-    blob.left = blob.left + 2
+    blob.left = blob.left + 5
     if blob.left > WIDTH:
         blob.right = 0
